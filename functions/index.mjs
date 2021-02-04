30e4aa2b2d742b29a4a947eb38c3ecc89f85961b
joshua mshana <mama27j@gmail.com>
2021-02-04 20:40:42 +0300

    now you can create a page and add its properties

--- functions/index.mjs
+++ functions/index.mjs
@@ -3,6 +3,6 @@ import bfastnode from 'bfastnode';
 export const bfastUiHome = bfastnode.bfast.functions().onHttpRequest(
     '/',
     (request, response) => {
-        response.json({message: 'Welcome to Bfast Ui'})
+        response.redirect('/project');
     }
 );
