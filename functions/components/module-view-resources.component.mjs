33dc89686d4118df3f8d8e16055d6dcfb737b772
joshua mshana <mama27j@gmail.com>
2021-02-05 10:22:48 +0300

    add service inject in feature module

--- functions/components/module-view-resources.component.mjs
+++ functions/components/module-view-resources.component.mjs
@@ -1,4 +1,5 @@
 import {errorMessageComponent} from "./error-message.component.mjs";
+import {moduleInjectionsTableComponent} from "./module-injections-table.component.mjs";
 
 /**
  *
@@ -6,9 +7,11 @@ import {errorMessageComponent} from "./error-message.component.mjs";
  * @param moduleName - {string}
  * @param project - {string} project name
  * @param mainModuleContents - {string} project main module contents
+ * @param injections {Array<*>}
+ * @param services {Array<*>}
  * @returns {string}
  */
-export const moduleViewResources = function (error, moduleName, project, mainModuleContents) {
+export const moduleViewResources = async function (error, moduleName, project, mainModuleContents, injections = [], services = []) {
     return `
             <div class="container col-xl-9 col-lg-9 col-sm-12 col-md-10 col-9" style="margin-top: 24px">
             
@@ -61,58 +64,62 @@ export const moduleViewResources = function (error, moduleName, project, mainMod
                 </div>
                 
                 <hr>
+                ${await moduleInjectionsTableComponent(project, moduleName, injections, services)}
                 
-                <div class="d-flex flex-row" style="align-items: center; margin: 8px 0">
-                    <h2 style="margin: 0">${moduleName}.module.ts</h2>
-                    <span style="flex: 1 1 auto"></span>
-                    <button id="saveCode" onclick="reload()" class="btn btn-primary">Reload</button>
-                    <div style="display: none" id="saveProgress" class="spinner-border text-primary" role="status">
-                      <span class="visually-hidden">Loading...</span>
-                    </div>
-                </div>
+                <hr>
                 
-                <div style="height: 80vh" id="moduleCode"></div>
-                <script src="/assets/editor/min/vs/loader.js"></script>
-                <script>
-                    function reload(){
-                        location.reload();
-                    }
-                    function saveCode(){
-                        // document.getElementById('saveProgress').setAttribute('style','display:block');
-                        // document.getElementById('saveCode').setAttribute('style','display:none');
-                        // const code = editor.getValue();
-                        // fetch('/module', {
-                        //     method: 'POST',
-                        //     headers: {
-                        //         'content-type': 'application/json'
-                        //     },
-                        //     body: JSON.stringify({
-                        //         code: code
-                        //     })
-                        // }).then(value => {
-                        //     if ()
-                        //     return value.json()
-                        // }).then(value => {
-                        //     console.log(value);
-                        // }).catch(reason => {
-                        //     console.log(reason);
-                        // }).finally(() => {
-                        //     document.getElementById('saveProgress').setAttribute('style','display:none');
-                        //     document.getElementById('saveCode').setAttribute('style','display:block');
-                        // });
-                    }
-                    require.config({ paths: { vs: '/assets/editor/min/vs' } });
-                    let  editor;
-                    require(['vs/editor/editor.main'], function () {
-                    editor = monaco.editor.create(document.getElementById('moduleCode'), {
-                        value: ${JSON.stringify(mainModuleContents)},
-                        language: 'typescript',
-                        theme: 'vs-dark',
-                        readOnly: true
-                        });
-                     document.getElementById('saveCode').setAttribute('style','display:block');
-                    });
-                </script>
-            </div>
-        `
+<!--                <div class="d-flex flex-row" style="align-items: center; margin: 8px 0">-->
+<!--                    <h2 style="margin: 0">${moduleName}.module.ts</h2>-->
+<!--                    <span style="flex: 1 1 auto"></span>-->
+<!--                    <button id="saveCode" onclick="reload()" class="btn btn-primary">Reload</button>-->
+<!--                    <div style="display: none" id="saveProgress" class="spinner-border text-primary" role="status">-->
+<!--                      <span class="visually-hidden">Loading...</span>-->
+<!--                    </div>-->
+<!--                </div>-->
+<!--                -->
+<!--                <div style="height: 80vh" id="moduleCode"></div>-->
+<!--                <script src="/assets/editor/min/vs/loader.js"></script>-->
+<!--                <script>-->
+<!--                    function reload(){-->
+<!--                        location.reload();-->
+<!--                    }-->
+<!--                    function saveCode(){-->
+<!--                        // document.getElementById('saveProgress').setAttribute('style','display:block');-->
+<!--                        // document.getElementById('saveCode').setAttribute('style','display:none');-->
+<!--                        // const code = editor.getValue();-->
+<!--                        // fetch('/module', {-->
+<!--                        //     method: 'POST',-->
+<!--                        //     headers: {-->
+<!--                        //         'content-type': 'application/json'-->
+<!--                        //     },-->
+<!--                        //     body: JSON.stringify({-->
+<!--                        //         code: code-->
+<!--                        //     })-->
+<!--                        // }).then(value => {-->
+<!--                        //     if ()-->
+<!--                        //     return value.json()-->
+<!--                        // }).then(value => {-->
+<!--                        //     console.log(value);-->
+<!--                        // }).catch(reason => {-->
+<!--                        //     console.log(reason);-->
+<!--                        // }).finally(() => {-->
+<!--                        //     document.getElementById('saveProgress').setAttribute('style','display:none');-->
+<!--                        //     document.getElementById('saveCode').setAttribute('style','display:block');-->
+<!--                        // });-->
+<!--                    }-->
+<!--                    require.config({ paths: { vs: '/assets/editor/min/vs' } });-->
+<!--                    let  editor;-->
+<!--                    require(['vs/editor/editor.main'], function () {-->
+<!--                    editor = monaco.editor.create(document.getElementById('moduleCode'), {-->
+<!--                        value: ${JSON.stringify(mainModuleContents)},-->
+<!--                        language: 'typescript',-->
+<!--                        theme: 'vs-dark',-->
+<!--                        readOnly: true-->
+<!--                        });-->
+<!--                     document.getElementById('saveCode').setAttribute('style','display:block');-->
+<!--                    });-->
+<!--                </script>-->
+      </div>
+<!-- -->        
+`
 }
